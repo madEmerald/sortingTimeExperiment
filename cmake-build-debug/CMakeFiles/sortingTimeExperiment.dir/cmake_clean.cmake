2a9b7e827db27ed3149bccc4a6ba7267000fb61c
SolntsevNS <nikitasergsol@yandex.ru>
2022-03-14 10:20:17 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/sortingTimeExperiment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortingTimeExperiment.dir/main.c.obj"
+  "libsortingTimeExperiment.dll.a"
+  "sortingTimeExperiment.exe"
+  "sortingTimeExperiment.exe.manifest"
+  "sortingTimeExperiment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sortingTimeExperiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sortingTimeExperiment.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sortingTimeExperiment.
+# This may be replaced when dependencies are built.
